7cd9609d2013b3cfea46ac7835637c5072baef2c
wuenyou <1293596367@qq.com>
2023-12-05 18:42:32 +0800

    故障库管理问题修改

--- src/views/avic/mms/tpm/tpmfailurewarehouse/TpmFailureWarehouseDetail.vue
+++ src/views/avic/mms/tpm/tpmfailurewarehouse/TpmFailureWarehouseDetail.vue
@@ -1,145 +1,126 @@
 <template>
   <div>
     <a-form
-      ref="formRef"
-      :model="form"
-      v-bind="layout"
-      class="form-excel-style"
+        ref="formRef"
+        :model="form"
+        v-bind="layout"
+        class="form-excel-style"
     >
       <a-row>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('billNo')">
           <a-form-item name="billNo" label="单据号" :rules="fieldRequired('billNo')" has-feedback>
-            <!--            <AvicAutoCode-->
-            <!--              v-model:value="form.billNo"-->
-            <!--              ref="autoCode"-->
-            <!--              code-type="FAILURE_BILL_NO"-->
-            <!--              code-param="TPM_FAILURE_WAREHOUSE"-->
-            <!--              :allow-clear="true"-->
-            <!--              :disabled="fieldDisabled('billNo')"-->
-            <!--              placeholder="请输入单据号"-->
-            <!--            />-->
             <a-input
-              v-model:value="form.billNo"
-              :auto-focus="true"
-              :disabled="fieldDisabled('billNo')"
+                v-model:value="form.billNo"
+                :auto-focus="true"
+                :disabled="fieldDisabled('billNo')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('equipmentCode')">
           <a-form-item
-            name="equipmentCode"
-            label="设备编号"
-            :rules="fieldRequired('equipmentCode')"
-            has-feedback
+              name="equipmentCode"
+              label="设备编号"
+              :rules="fieldRequired('equipmentCode')"
+              has-feedback
           >
-            <AvicModalSelect
-              v-model:value="form.equipmentCode"
-              :defaultShowValue="form.equipmentCode"
-              modalTitle="选择设备编号"
-              placeholder="请选择设备编号"
-              valueField=""
-              showField=""
-              :selectComponent="tpmInventorySelectComponent"
-              :isMultiSelection="false"
-              :allow-clear="true"
-              :disabled="fieldDisabled('equipmentCode')"
-            />
+            <a-input v-model:value="form.equipmentCode" :maxLength="64" :auto-focus="true"
+                     placeholder="请选择设备编号" :readonly="true" @click="handleOpen"/>
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('equipmentName')">
           <a-form-item name="equipmentName" label="设备名称" :rules="fieldRequired('equipmentName')" has-feedback>
             <a-input
-              v-model:value="form.equipmentName"
-              :auto-focus="true"
-              :disabled="fieldDisabled('equipmentName')"
+                v-model:value="form.equipmentName"
+                :auto-focus="true"
+                :disabled="fieldDisabled('equipmentName')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('model')">
           <a-form-item name="model" label="设备型号" :rules="fieldRequired('model')" has-feedback>
             <a-input
-              v-model:value="form.model"
-              :disabled="fieldDisabled('model')"
+                v-model:value="form.model"
+                :disabled="fieldDisabled('model')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('specs')">
           <a-form-item name="specs" label="设备规格" :rules="fieldRequired('specs')" has-feedback>
             <a-input
-              v-model:value="form.specs"
-              :disabled="fieldDisabled('specs')"
+                v-model:value="form.specs"
+                :disabled="fieldDisabled('specs')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('useDeptName')">
           <a-form-item name="useDeptName" label="使用部门名称" :rules="fieldRequired('useDeptName')" has-feedback>
             <a-input
-              v-model:value="form.useDeptName"
-              :disabled="fieldDisabled('useDeptName')"
+                v-model:value="form.useDeptName"
+                :disabled="fieldDisabled('useDeptName')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('applyUserName')">
           <a-form-item name="applyUserName" label="申请人姓名" :rules="fieldRequired('applyUserName')" has-feedback>
             <AvicCommonSelect
-              v-model:value="form.applyUserName"
-              type="userSelect"
-              :defaultShowValue="form.applyUserNameAlias"
-              :disabled="fieldDisabled('applyUserName')"
+                v-model:value="form.applyUserName"
+                type="userSelect"
+                :defaultShowValue="form.applyUserNameAlias"
+                :disabled="fieldDisabled('applyUserName')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('applyUserTel')">
           <a-form-item name="applyUserTel" label="申请人电话" :rules="fieldRequired('applyUserTel')" has-feedback>
             <a-input
-              v-model:value="form.applyUserTel"
-              :disabled="fieldDisabled('applyUserTel')"
+                v-model:value="form.applyUserTel"
+                :disabled="fieldDisabled('applyUserTel')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('repairDept')">
           <a-form-item name="repairDept" label="维修单位" :rules="fieldRequired('repairDept')" has-feedback>
             <a-input
-              v-model:value="form.repairDept"
-              :disabled="fieldDisabled('repairDept')"
+                v-model:value="form.repairDept"
+                :disabled="fieldDisabled('repairDept')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('applyDate')">
           <a-form-item name="applyDate" label="申请日期" :rules="fieldRequired('applyDate')" has-feedback>
             <a-date-picker
-              v-model:value="form.applyDate"
-              format="YYYY-MM-DD"
-              value-format="YYYY-MM-DD"
-              :disabled="fieldDisabled('applyDate')"
+                v-model:value="form.applyDate"
+                format="YYYY-MM-DD"
+                value-format="YYYY-MM-DD"
+                :disabled="fieldDisabled('applyDate')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('faultDate')">
           <a-form-item name="faultDate" label="故障发生时间" :rules="fieldRequired('faultDate')" has-feedback>
             <a-date-picker
-              v-model:value="form.faultDate"
-              format="YYYY-MM-DD"
-              value-format="YYYY-MM-DD"
-              :disabled="fieldDisabled('faultDate')"
+                v-model:value="form.faultDate"
+                format="YYYY-MM-DD"
+                value-format="YYYY-MM-DD"
+                :disabled="fieldDisabled('faultDate')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('planFinishDate')">
           <a-form-item name="planFinishDate" label="计划完成时间" :rules="fieldRequired('planFinishDate')" has-feedback>
             <a-date-picker
-              v-model:value="form.planFinishDate"
-              format="YYYY-MM-DD"
-              value-format="YYYY-MM-DD"
-              :disabled="fieldDisabled('planFinishDate')"
+                v-model:value="form.planFinishDate"
+                format="YYYY-MM-DD"
+                value-format="YYYY-MM-DD"
+                :disabled="fieldDisabled('planFinishDate')"
             />
           </a-form-item>
         </a-col>
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('repairMoney')">
           <a-form-item name="repairMoney" label="维修金额" :rules="fieldRequired('repairMoney')" has-feedback>
             <a-input
-              v-model:value="form.repairMoney"
-              :disabled="fieldDisabled('repairMoney')"
+                v-model:value="form.repairMoney"
+                :disabled="fieldDisabled('repairMoney')"
             />
           </a-form-item>
         </a-col>
@@ -147,17 +128,17 @@
           <a-form-item name="failureWarehouseType" label="故障库类型" :rules="fieldRequired('failureWarehouseType')"
                        has-feedback>
             <a-select
-              v-model:value="form.failureWarehouseType"
-              :get-popup-container="triggerNode => triggerNode.parentNode"
-              option-filter-prop="children"
-              :show-search="true"
-              :allow-clear="true"
-              :disabled="fieldDisabled('failureWarehouseType')"
+                v-model:value="form.failureWarehouseType"
+                :get-popup-container="triggerNode => triggerNode.parentNode"
+                option-filter-prop="children"
+                :show-search="true"
+                :allow-clear="true"
+                :disabled="fieldDisabled('failureWarehouseType')"
             >
               <a-select-option
-                v-for="item in failureWarehouseTypeList"
-                :key="item.sysLookupTlId"
-                :value="item.lookupCode"
+                  v-for="item in failureWarehouseTypeList"
+                  :key="item.sysLookupTlId"
+                  :value="item.lookupCode"
               >
                 {{ item.lookupName }}
               </a-select-option>
@@ -167,17 +148,17 @@
         <a-col v-bind="colLayout.cols" v-if="fieldVisible('secretLevel')">
           <a-form-item name="secretLevel" label="密级" :rules="fieldRequired('secretLevel')" has-feedback>
             <a-select
-              v-model:value="form.secretLevel"
-              :get-popup-container="triggerNode => triggerNode.parentNode"
-              option-filter-prop="children"
-              :show-search="true"
-              :allow-clear="true"
-              :disabled="fieldDisabled('secretLevel')"
+                v-model:value="form.secretLevel"
+                :get-popup-container="triggerNode => triggerNode.parentNode"
+                option-filter-prop="children"
+                :show-search="true"
+                :allow-clear="true"
+                :disabled="fieldDisabled('secretLevel')"
             >
               <a-select-option
-                v-for="item in secretLevelList"
-                :key="item.sysLookupTlId"
-                :value="item.lookupCode"
+                  v-for="item in secretLevelList"
+                  :key="item.sysLookupTlId"
+                  :value="item.lookupCode"
               >
                 {{ item.lookupName }}
               </a-select-option>
@@ -201,24 +182,32 @@
         <a-col v-bind="colLayout.cols3">
           <a-form-item label="上传附件" type="attachment" :rules="attachmentRequired('uploadFile')">
             <AvicUploader
-              element-id="1"
-              ref="uploadFile"
-              label="上传附件"
-              :form-id="form.id"
-              :bpm-instance-object="props.bpmInstanceObject"
-              :form-secret-level="form.secretLevel"
-              :allow-download="true"
-              table-name="TPM_FAILURE_WAREHOUSE"
-              @afterUpload="afterUploadEvent"
+                element-id="1"
+                ref="uploadFile"
+                label="上传附件"
+                :form-id="form.id"
+                :bpm-instance-object="props.bpmInstanceObject"
+                :form-secret-level="form.secretLevel"
+                :allow-download="true"
+                table-name="TPM_FAILURE_WAREHOUSE"
+                @afterUpload="afterUploadEvent"
             />
           </a-form-item>
         </a-col>
       </a-row>
     </a-form>
+    <!--选择设备弹窗-->
+    <a-modal :visible="open" title="选择设备" @ok="handleOk" @cancel="handleCancel" width="80%" style="top: 20px">
+      <div style="height: 400px;overflow: auto">
+        <Tpm-inventory-failure-select ref="tpmInventoryFailureSelect">
+        </Tpm-inventory-failure-select>
+      </div>
+    </a-modal>
   </div>
 </template>
 <script lang="ts" setup>
-import { useTpmFailureWarehouseForm, emits } from './ts/TpmFailureWarehouseForm'; // 引入表单ts
+import { useTpmFailureWarehouseForm, emits } from './ts/TpmFailureWarehouseForm';
+import TpmInventoryFailureSelect from '@/views/avic/mms/tpm/tpmfailurewarehouse/TpmInventoryFailureSelect.vue'; // 引入表单ts
 const props = defineProps({
   formId: {
     type: String,
@@ -266,4 +255,27 @@ defineExpose({
   beforeClickBpmButtons,
   afterClickBpmButtons
 });
+const open = ref<boolean>(false);
+const tpmInventoryFailureSelect = ref(null);
+
+function handleOpen() {
+  open.value = true;
+}
+
+const handleCancel = () => {
+  open.value = false;
+};
+
+const handleOk = () => {
+  const info = tpmInventoryFailureSelect.value.info;
+  form.value.equipmentCode = info.equipmentCode;
+  form.value.equipmentName = info.equipmentName;
+  form.value.specs = info.specs;
+  form.value.model = info.model;
+  form.value.useDeptName = info.useDeptName;
+  form.value.useDeptId = info.useDeptId;
+  form.value.useDeptCode = info.useDeptCode;
+  form.value['tpmInventoryId'] = info.id;
+  open.value = false;
+};
 </script>
--- src/views/avic/mms/tpm/tpmfailurewarehouse/TpmFailureWarehouseEdit.vue
+++ src/views/avic/mms/tpm/tpmfailurewarehouse/TpmFailureWarehouseEdit.vue
@@ -1,158 +1,150 @@
 <template>
   <AvicModal
-    :visible="true"
-    title="编辑"
-    width="1200px"
-    height="520px"
-    :centered="true"
-    @cancel="closeModal"
+      :visible="true"
+      title="编辑"
+      width="1200px"
+      height="520px"
+      :centered="true"
+      @cancel="closeModal"
   >
     <a-spin :spinning="loading">
       <a-form
-        ref="formRef"
-        :model="form"
-        :rules="rules"
-        v-bind="layout"
-        class="form-excel-style"
-       >
+          ref="formRef"
+          :model="form"
+          :rules="rules"
+          v-bind="layout"
+          class="form-excel-style"
+      >
         <a-row>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="billNo" label="单据号" has-feedback>
-              <a-input v-model:value="form.billNo" disabled />
+              <a-input v-model:value="form.billNo" disabled/>
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="equipmentCode" label="设备编号" has-feedback>
-              <AvicModalSelect
-                v-model:value="form.equipmentCode"
-                title="选择设备编号"
-                placeholder="请选择设备编号"
-                valueField=""
-                showField=""
-                :selectComponent="tpmInventorySelectComponent"
-                :defaultShowValue="form.equipmentCode"
-                :allow-clear="true"
-              />
+              <a-input v-model:value="form.equipmentCode" :maxLength="64" :auto-focus="true"
+                       placeholder="请选择设备编号" :readonly="true" @click="handleOpen"/>
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="equipmentName" label="设备名称" has-feedback>
               <a-input
-                v-model:value="form.equipmentName"
-                :maxLength="128"
-                :auto-focus="true"
-                placeholder="请输入设备名称"
+                  v-model:value="form.equipmentName"
+                  :maxLength="128"
+                  :auto-focus="true"
+                  placeholder="请输入设备名称"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="model" label="设备型号" has-feedback>
               <a-input
-                v-model:value="form.model"
-                :maxLength="128"
-                placeholder="请输入设备型号"
+                  v-model:value="form.model"
+                  :maxLength="128"
+                  placeholder="请输入设备型号"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="specs" label="设备规格" has-feedback>
               <a-input
-                v-model:value="form.specs"
-                :maxLength="128"
-                placeholder="请输入设备规格"
+                  v-model:value="form.specs"
+                  :maxLength="128"
+                  placeholder="请输入设备规格"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="useDeptName" label="使用部门名称" has-feedback>
               <a-input
-                v-model:value="form.useDeptName"
-                :maxLength="128"
-                placeholder="请输入使用部门名称"
+                  v-model:value="form.useDeptName"
+                  :maxLength="128"
+                  placeholder="请输入使用部门名称"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="applyUserId" label="申请人姓名" has-feedback>
               <AvicCommonSelect
-                v-model:value="form.applyUserId"
-                type="userSelect"
-                placeholder="请选择申请人姓名"
-                :defaultShowValue="form.applyUserNameAlias"
+                  v-model:value="form.applyUserId"
+                  type="userSelect"
+                  placeholder="请选择申请人姓名"
+                  :defaultShowValue="form.applyUserNameAlias"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="applyUserTel" label="申请人电话">
               <a-input
-                v-model:value="form.applyUserTel"
-                :maxLength="64"
-                placeholder="请输入申请人电话"
+                  v-model:value="form.applyUserTel"
+                  :maxLength="64"
+                  placeholder="请输入申请人电话"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="repairDept" label="维修单位">
               <a-input
-                v-model:value="form.repairDept"
-                :maxLength="128"
-                placeholder="请输入维修单位"
+                  v-model:value="form.repairDept"
+                  :maxLength="128"
+                  placeholder="请输入维修单位"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="applyDate" label="申请日期" has-feedback>
               <a-date-picker
-                v-model:value="form.applyDate"
-                format="YYYY-MM-DD"
-                value-format="YYYY-MM-DD"
-                placeholder="请选择申请日期"
+                  v-model:value="form.applyDate"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD"
+                  placeholder="请选择申请日期"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="faultDate" label="故障发生时间" has-feedback>
               <a-date-picker
-                v-model:value="form.faultDate"
-                format="YYYY-MM-DD"
-                value-format="YYYY-MM-DD"
-                placeholder="请选择故障发生时间"
+                  v-model:value="form.faultDate"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD"
+                  placeholder="请选择故障发生时间"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="planFinishDate" label="计划完成时间" has-feedback>
               <a-date-picker
-                v-model:value="form.planFinishDate"
-                format="YYYY-MM-DD"
-                value-format="YYYY-MM-DD"
-                placeholder="请选择计划完成时间"
+                  v-model:value="form.planFinishDate"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD"
+                  placeholder="请选择计划完成时间"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="repairMoney" label="维修金额">
               <a-input
-                v-model:value="form.repairMoney"
-                :maxLength="20"
-                placeholder="请输入维修金额"
+                  v-model:value="form.repairMoney"
+                  :maxLength="20"
+                  placeholder="请输入维修金额"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols">
             <a-form-item name="failureWarehouseType" label="故障库类型" has-feedback>
               <a-select
-                v-model:value="form.failureWarehouseType"
-                :get-popup-container="triggerNode => triggerNode.parentNode"
-                option-filter-prop="children"
-                :show-search="true"
-                :allow-clear="true"
-                placeholder="请选择故障库类型"
+                  v-model:value="form.failureWarehouseType"
+                  :get-popup-container="triggerNode => triggerNode.parentNode"
+                  option-filter-prop="children"
+                  :show-search="true"
+                  :allow-clear="true"
+                  placeholder="请选择故障库类型"
               >
                 <a-select-option
-                  v-for="item in failureWarehouseTypeList"
-                  :key="item.sysLookupTlId"
-                  :value="item.lookupCode"
+                    v-for="item in failureWarehouseTypeList"
+                    :key="item.sysLookupTlId"
+                    :value="item.lookupCode"
                 >
                   {{ item.lookupName }}
                 </a-select-option>
@@ -162,17 +154,17 @@
           <a-col v-bind="colLayout.cols">
             <a-form-item name="secretLevel" label="密级" has-feedback>
               <a-select
-                v-model:value="form.secretLevel"
-                :get-popup-container="triggerNode => triggerNode.parentNode"
-                option-filter-prop="children"
-                :show-search="true"
-                :allow-clear="true"
-                placeholder="请选择密级"
+                  v-model:value="form.secretLevel"
+                  :get-popup-container="triggerNode => triggerNode.parentNode"
+                  option-filter-prop="children"
+                  :show-search="true"
+                  :allow-clear="true"
+                  placeholder="请选择密级"
               >
                 <a-select-option
-                  v-for="item in secretLevelList"
-                  :key="item.sysLookupTlId"
-                  :value="item.lookupCode"
+                    v-for="item in secretLevelList"
+                    :key="item.sysLookupTlId"
+                    :value="item.lookupCode"
                 >
                   {{ item.lookupName }}
                 </a-select-option>
@@ -183,14 +175,14 @@
         <a-row>
           <a-col v-bind="colLayout.cols3">
             <a-form-item
-              name="faultContent"
-              label="故障现象及原因"
+                name="faultContent"
+                label="故障现象及原因"
             >
               <a-textarea
-                v-model:value="form.faultContent"
-                :rows="2"
-                :maxLength="4000"
-                placeholder="请输入故障现象及原因"
+                  v-model:value="form.faultContent"
+                  :rows="2"
+                  :maxLength="4000"
+                  placeholder="请输入故障现象及原因"
               />
             </a-form-item>
           </a-col>
@@ -198,33 +190,33 @@
         <a-row>
           <a-col v-bind="colLayout.cols3">
             <a-form-item
-              name="repairContent"
-              label="维修内容"
+                name="repairContent"
+                label="维修内容"
             >
               <a-textarea
-                v-model:value="form.repairContent"
-                :rows="2"
-                :maxLength="4000"
-                placeholder="请输入维修内容"
+                  v-model:value="form.repairContent"
+                  :rows="2"
+                  :maxLength="4000"
+                  placeholder="请输入维修内容"
               />
             </a-form-item>
           </a-col>
           <a-col v-bind="colLayout.cols3">
             <a-form-item
-              label="附件"
+                label="附件"
             >
               <AvicUploader
-                element-id="1"
-                form-type="edit"
-                ref="uploadFile"
-                :allow-download="true"
-                :allow-preview="true"
-                :allow-delete="true"
-                :allow-update-secret-level="true"
-                :form-id="form.id"
-                :form-secret-level="form.secretLevel"
-                table-name="TPM_FAILURE_WAREHOUSE"
-                @afterUpload="afterUploadEvent"
+                  element-id="1"
+                  form-type="edit"
+                  ref="uploadFile"
+                  :allow-download="true"
+                  :allow-preview="true"
+                  :allow-delete="true"
+                  :allow-update-secret-level="true"
+                  :form-id="form.id"
+                  :form-secret-level="form.secretLevel"
+                  table-name="TPM_FAILURE_WAREHOUSE"
+                  @afterUpload="afterUploadEvent"
               />
             </a-form-item>
           </a-col>
@@ -235,11 +227,19 @@
       <a-button title="保存" type="primary" :loading="loading" @click="saveForm">保存</a-button>
       <a-button title="返回" type="primary" ghost @click="closeModal">返回</a-button>
     </template>
+    <!--选择设备弹窗-->
+    <a-modal :visible="open" title="选择设备" @ok="handleOk" @cancel="handleCancel" width="80%" style="top: 20px">
+      <div style="height: 400px;overflow: auto">
+        <Tpm-inventory-failure-select ref="tpmInventoryFailureSelect">
+        </Tpm-inventory-failure-select>
+      </div>
+    </a-modal>
   </AvicModal>
 </template>
 <script lang="ts" setup>
 import { useTpmFailureWarehouseForm, emits } from './ts/TpmFailureWarehouseForm'; // 引入表单ts
-import tpmInventorySelect from '@/views/avic/mms/tpm/tpminventory/TpmInventorySelect.vue'; // 引入弹窗选择页
+import TpmInventoryFailureSelect from '@/views/avic/mms/tpm/tpmfailurewarehouse/TpmInventoryFailureSelect.vue'; // 引入弹窗选择页
+
 const props = defineProps({
   formId: {
     type: String,
@@ -258,7 +258,6 @@ const props = defineProps({
   }
 });
 const emit = defineEmits(emits);
-const tpmInventorySelectComponent= tpmInventorySelect;// 自定义选择
 const {
   form,
   formRef,
@@ -277,4 +276,27 @@ const {
   props: props,
   emit: emit
 });
+const open = ref<boolean>(false);
+const tpmInventoryFailureSelect = ref(null);
+
+function handleOpen() {
+  open.value = true;
+}
+
+const handleCancel = () => {
+  open.value = false;
+};
+
+const handleOk = () => {
+  const info = tpmInventoryFailureSelect.value.info;
+  form.value.equipmentCode = info.equipmentCode;
+  form.value.equipmentName = info.equipmentName;
+  form.value.specs = info.specs;
+  form.value.model = info.model;
+  form.value.useDeptName = info.useDeptName;
+  form.value.useDeptId = info.useDeptId;
+  form.value.useDeptCode = info.useDeptCode;
+  form.value['tpmInventoryId'] = info.id;
+  open.value = false;
+};
 </script>
