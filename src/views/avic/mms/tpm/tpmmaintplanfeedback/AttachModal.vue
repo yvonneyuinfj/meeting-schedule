5a5ef9eb581aa095b62eeec3dd7b0fb6df29d7e7
baih <123456789@qq.com>
2023-12-15 09:04:58 +0800

    设备保养计划反馈

---
+++ src/views/avic/mms/tpm/tpmmaintplanfeedback/AttachModal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content-wrapper">
+    <a-textarea
+        v-model:value="note"
+        :rows="10"
+        :maxLength="512"
+        placeholder="请输入备注"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+const note = ref();
+defineExpose({
+  note
+});
+</script>
