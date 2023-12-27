42dde7ecf201dd05b9ee9bf3de05bca7a947e3d2
wangd <wangd@avic.com>
2023-12-27 18:34:19 +0800

    资产封存/启封申请(日期关系校验)

--- src/views/avic/mms/fam/famsealandunseal/FamSealAndUnsealAdd.vue
+++ src/views/avic/mms/fam/famsealandunseal/FamSealAndUnsealAdd.vue
@@ -1,179 +1,112 @@
 <template>
-  <AvicModal
-    :visible="true"
-    title="添加"
-    width="960px"
-    height="520px"
-    :centered="true"
-    @cancel="closeModal"
-  >
-    <a-spin :spinning="loading">
-      <a-form ref="formRef" :model="form" :rules="rules" v-bind="layout" class="form-excel-style">
-        <a-row :gutter="0">
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="applyNo" label="申请单编号" has-feedback>
-              <avic-auto-code
-                v-model:value="form.applyNo"
-                ref="autoCode"
-                code-type="FAM_BILL_NO"
-                code-param="FAM_SEAL_AND_UNSEAL"
-                :allow-clear="true"
-                :disabled="false"
-                placeholder="请输入申请单编号"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="title" label="标题">
-              <a-input
-                v-model:value="form.title"
-                :maxLength="64"
-                :auto-focus="true"
-                placeholder="请输入标题"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="sealingType" label="封存类型">
-              <a-select
-                v-model:value="form.sealingType"
-                :get-popup-container="triggerNode => triggerNode.parentNode"
-                option-filter-prop="children"
-                :show-search="true"
-                :allow-clear="true"
-                placeholder="请选择封存类型"
-              >
-                <a-select-option
-                  v-for="item in sealingTypeList"
-                  :key="item.sysLookupTlId"
-                  :value="item.lookupCode"
-                >
-                  {{ item.lookupName }}
-                </a-select-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="isAssetIntact" label="资产是否完好">
-              <a-select
-                v-model:value="form.isAssetIntact"
-                :get-popup-container="triggerNode => triggerNode.parentNode"
-                option-filter-prop="children"
-                :show-search="true"
-                :allow-clear="true"
-                placeholder="请选择资产是否完好"
-              >
-                <a-select-option
-                  v-for="item in isAssetIntactList"
-                  :key="item.sysLookupTlId"
-                  :value="item.lookupCode"
-                >
-                  {{ item.lookupName }}
-                </a-select-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="managerDeptId" label="主管部门">
-              <AvicCommonSelect
-                v-model:value="form.managerDeptId"
-                type="deptSelect"
-                placeholder="请选择主管部门"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="sealingStartDate" label="封存开始日期">
-              <a-date-picker
-                v-model:value="form.sealingStartDate"
-                format="YYYY-MM-DD"
-                value-format="YYYY-MM-DD"
-                placeholder="请选择封存开始日期"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="sealingEndDate" label="封存终止日期">
-              <a-date-picker
-                v-model:value="form.sealingEndDate"
-                format="YYYY-MM-DD"
-                value-format="YYYY-MM-DD"
-                placeholder="请选择封存终止日期"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="unsealStartDate" label="启封开始日期">
-              <a-date-picker
-                v-model:value="form.unsealStartDate"
-                format="YYYY-MM-DD"
-                value-format="YYYY-MM-DD"
-                placeholder="请选择启封开始日期"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="applyReason" label="申请理由">
-              <a-input
-                v-model:value="form.applyReason"
-                :maxLength="64"
-                placeholder="请输入申请理由"
-              />
-            </a-form-item>
-          </a-col>
-<!--          <a-col v-bind="colLayout.cols">-->
-<!--            <a-form-item name="otherMatter" label="其他事项">-->
-<!--              <a-input-->
-<!--                v-model:value="form.otherMatter"-->
-<!--                :maxLength="64"-->
-<!--                placeholder="请输入其他事项"-->
-<!--              />-->
-<!--            </a-form-item>-->
-<!--          </a-col>-->
-<!--          <a-col v-bind="colLayout.cols">-->
-<!--            <a-form-item name="sealingExecuteStatus" label="封存规定执行情况">-->
-<!--              <a-input-->
-<!--                v-model:value="form.sealingExecuteStatus"-->
-<!--                :maxLength="64"-->
-<!--                placeholder="请输入封存规定执行情况"-->
-<!--              />-->
-<!--            </a-form-item>-->
-<!--          </a-col>-->
-<!--          <a-col v-bind="colLayout.cols">-->
-<!--            <a-form-item name="unsealExecuteStatus" label="启封执行情况">-->
-<!--              <a-input-->
-<!--                v-model:value="form.unsealExecuteStatus"-->
-<!--                :maxLength="64"-->
-<!--                placeholder="请输入启封执行情况"-->
-<!--              />-->
-<!--            </a-form-item>-->
-<!--          </a-col>-->
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="applyDeptId" label="申请部门" has-feedback>
-              <AvicCommonSelect
-                v-model:value="form.applyDeptId"
-                type="deptSelect"
-                placeholder="请选择申请部门"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="handlePersonId" label="经办人" has-feedback>
-              <AvicCommonSelect
-                v-model:value="form.handlePersonId"
-                type="userSelect"
-                placeholder="请选择经办人"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col v-bind="colLayout.cols">
-            <a-form-item name="applyDate" label="申请时间" has-feedback>
-              <a-date-picker
-                v-model:value="form.applyDate"
-                format="YYYY-MM-DD"
-                value-format="YYYY-MM-DD"
-                placeholder="请选择申请时间"
-              />
+          <AvicModal :visible="true" title="添加" width="960px" height="520px" :centered="true" @cancel="closeModal">
+            <a-spin :spinning="loading">
+              <a-form ref="formRef" :model="form" :rules="rules" v-bind="layout" class="form-excel-style">
+                <a-row :gutter="0">
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="applyNo" label="申请单编号" has-feedback>
+                      <avic-auto-code v-model:value="form.applyNo" ref="autoCode" code-type="FAM_BILL_NO"
+                        code-param="FAM_SEAL_AND_UNSEAL" :allow-clear="true" :disabled="false" placeholder="请输入申请单编号" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="title" label="标题">
+                      <a-input v-model:value="form.title" :maxLength="64" :auto-focus="true" placeholder="请输入标题" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="sealingType" label="封存类型">
+                      <a-select v-model:value="form.sealingType" :get-popup-container="triggerNode => triggerNode.parentNode"
+                        option-filter-prop="children" :show-search="true" :allow-clear="true" placeholder="请选择封存类型">
+                        <a-select-option v-for="item in sealingTypeList" :key="item.sysLookupTlId" :value="item.lookupCode">
+                          {{ item.lookupName }}
+                        </a-select-option>
+                      </a-select>
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="isAssetIntact" label="资产是否完好">
+                      <a-select v-model:value="form.isAssetIntact" :get-popup-container="triggerNode => triggerNode.parentNode"
+                        option-filter-prop="children" :show-search="true" :allow-clear="true" placeholder="请选择资产是否完好">
+                        <a-select-option v-for="item in isAssetIntactList" :key="item.sysLookupTlId" :value="item.lookupCode">
+                          {{ item.lookupName }}
+                        </a-select-option>
+                      </a-select>
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="managerDeptId" label="主管部门">
+                      <AvicCommonSelect v-model:value="form.managerDeptId" type="deptSelect" placeholder="请选择主管部门" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="sealingStartDate" label="封存开始日期">
+                      <a-date-picker v-model:value="form.sealingStartDate" format="YYYY-MM-DD" value-format="YYYY-MM-DD"
+                        placeholder="请选择封存开始日期" :disabled-date="
+                          startValue => proxy.$disabledStartDate(startValue, form.sealingEndDate)
+                        " />
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="sealingEndDate" label="封存终止日期">
+                      <a-date-picker v-model:value="form.sealingEndDate" format="YYYY-MM-DD" value-format="YYYY-MM-DD"
+                        placeholder="请选择封存终止日期" :disabled-date="
+                          startValue => proxy.$disabledEndDate(startValue, form.sealingStartDate)" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="unsealStartDate" label="启封开始日期">
+                      <a-date-picker v-model:value="form.unsealStartDate" format="YYYY-MM-DD" value-format="YYYY-MM-DD"
+                        placeholder="请选择启封开始日期" :disabled-date="
+                          startValue => proxy.$disabledEndDate(startValue, form.sealingEndDate)"/>
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="applyReason" label="申请理由">
+                      <a-input v-model:value="form.applyReason" :maxLength="64" placeholder="请输入申请理由" />
+                    </a-form-item>
+                  </a-col>
+                  <!--          <a-col v-bind="colLayout.cols">-->
+                  <!--            <a-form-item name="otherMatter" label="其他事项">-->
+                  <!--              <a-input-->
+                  <!--                v-model:value="form.otherMatter"-->
+                  <!--                :maxLength="64"-->
+                  <!--                placeholder="请输入其他事项"-->
+                  <!--              />-->
+                  <!--            </a-form-item>-->
+                  <!--          </a-col>-->
+                  <!--          <a-col v-bind="colLayout.cols">-->
+                  <!--            <a-form-item name="sealingExecuteStatus" label="封存规定执行情况">-->
+                  <!--              <a-input-->
+                  <!--                v-model:value="form.sealingExecuteStatus"-->
+                  <!--                :maxLength="64"-->
+                  <!--                placeholder="请输入封存规定执行情况"-->
+                  <!--              />-->
+                  <!--            </a-form-item>-->
+                  <!--          </a-col>-->
+                  <!--          <a-col v-bind="colLayout.cols">-->
+                  <!--            <a-form-item name="unsealExecuteStatus" label="启封执行情况">-->
+                  <!--              <a-input-->
+                  <!--                v-model:value="form.unsealExecuteStatus"-->
+                  <!--                :maxLength="64"-->
+                  <!--                placeholder="请输入启封执行情况"-->
+                  <!--              />-->
+                  <!--            </a-form-item>-->
+                  <!--          </a-col>-->
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="applyDeptId" label="申请部门" has-feedback>
+                      <AvicCommonSelect v-model:value="form.applyDeptId" type="deptSelect" placeholder="请选择申请部门" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="handlePersonId" label="经办人" has-feedback>
+                      <AvicCommonSelect v-model:value="form.handlePersonId" type="userSelect" placeholder="请选择经办人" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col v-bind="colLayout.cols">
+                    <a-form-item name="applyDate" label="申请时间" has-feedback>
+                      <a-date-picker v-model:value="form.applyDate" format="YYYY-MM-DD" value-format="YYYY-MM-DD"
+                        placeholder="请选择申请时间" />
             </a-form-item>
           </a-col>
         </a-row>
@@ -189,7 +122,7 @@
 <script lang="ts" setup>
 import { useFamSealAndUnsealForm, emits } from './ts/FamSealAndUnsealForm'; // 引入表单ts
 import FamSealAndUnsealListEdit from '@/views/avic/mms/fam/famsealandunseallist/FamSealAndUnsealListEdit.vue'; // 引入子表组件
-
+const { proxy } = getCurrentInstance()
 const props = defineProps({
   formId: {
     type: String,
