bbe6f174dfb671068388bc4e5f1e064440dc4237
wuenyou <1293596367@qq.com>
2023-12-28 10:38:28 +0800

    mds:报表打印临时表：创建报表用户与表ID之间关系

---
+++ src/views/avic/mms/mds/mdsreportcondition/MdsReportConditionDetail.vue
@@ -0,0 +1,85 @@
+<template>
+  <AvicModal
+    :visible="true"
+    title="详情"
+    width="960px"
+    height="520px"
+    :centered="true"
+    @cancel="closeModal"
+  >
+    <a-spin :spinning="loading">
+      <a-form
+        ref="formRef"
+        :model="form"
+        v-bind="layout"
+        layout="horizontal"
+       >
+        <a-row :gutter="16">
+          <a-col v-bind="colLayout.cols">
+            <a-form-item name="sysUserId" label="用户ID">
+              <AvicCommonSelect
+                v-model:value="form.sysUserId"
+                :auto-focus="true"
+                type="userSelect"
+                :defaultShowValue="form.sysUserIdAlias"
+                disabled
+              />
+            </a-form-item>
+          </a-col>
+          <a-col v-bind="colLayout.cols">
+            <a-form-item name="tableId" label="传递表ID">
+              <a-input v-model:value="form.tableId" disabled />
+            </a-form-item>
+          </a-col>
+          <a-col v-bind="colLayout.cols">
+            <a-form-item name="secretLevel" label="密级 ^ 通用代码MMS_DATA_SECRET_LEVEL">
+              <a-select
+                v-model:value="form.secretLevel"
+                :get-popup-container="triggerNode => triggerNode.parentNode"
+                option-filter-prop="children"
+                :show-search="true"
+                :allow-clear="true"
+                disabled
+              >
+                <a-select-option
+                  v-for="item in secretLevelList"
+                  :key="item.sysLookupTlId"
+                  :value="item.lookupCode"
+                >
+                  {{ item.lookupName }}
+                </a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+    </a-spin>
+    <template #footer>
+      <a-button title="返回" type="primary" ghost @click="closeModal">返回</a-button>
+    </template>
+  </AvicModal>
+</template>
+<script lang="ts" setup>
+import { useMdsReportConditionForm, emits } from './ts/MdsReportConditionForm'; // 引入表单ts
+const props = defineProps({
+  formId: {
+    type: String,
+    default: ''
+  }
+});
+const emit = defineEmits(emits);
+const {
+  form,
+  formRef,
+  layout,
+  colLayout,
+  loading,
+  secretLevelList,
+  closeModal
+} = useMdsReportConditionForm({
+  props: props,
+  emit: emit
+});
+</script>
+
+
---
+++ src/views/avic/mms/mds/mdsreportcondition/MdsReportConditionEdit.vue
@@ -0,0 +1,93 @@
+<template>
+  <AvicModal
+    :visible="true"
+    title="编辑"
+    width="960px"
+    height="520px"
+    :centered="true"
+    @cancel="closeModal"
+  >
+    <a-spin :spinning="loading">
+      <a-form
+        ref="formRef"
+        :model="form"
+        :rules="rules"
+        v-bind="layout"
+        layout="horizontal"
+       >
+        <a-row :gutter="16">
+          <a-col v-bind="colLayout.cols">
+            <a-form-item name="sysUserId" label="用户ID">
+              <AvicCommonSelect
+                v-model:value="form.sysUserId"
+                type="userSelect"
+                placeholder="请选择用户ID"
+                :defaultShowValue="form.sysUserIdAlias"
+              />
+            </a-form-item>
+          </a-col>
+          <a-col v-bind="colLayout.cols">
+            <a-form-item name="tableId" label="传递表ID">
+              <a-input
+                v-model:value="form.tableId"
+                :maxLength="64"
+                :auto-focus="true"
+                placeholder="请输入传递表ID"
+              />
+            </a-form-item>
+          </a-col>
+          <a-col v-bind="colLayout.cols">
+            <a-form-item name="secretLevel" label="密级 ^ 通用代码MMS_DATA_SECRET_LEVEL" has-feedback>
+              <a-select
+                v-model:value="form.secretLevel"
+                :get-popup-container="triggerNode => triggerNode.parentNode"
+                option-filter-prop="children"
+                :show-search="true"
+                :allow-clear="true"
+                placeholder="请选择密级 ^ 通用代码MMS_DATA_SECRET_LEVEL"
+              >
+                <a-select-option
+                  v-for="item in secretLevelList"
+                  :key="item.sysLookupTlId"
+                  :value="item.lookupCode"
+                >
+                  {{ item.lookupName }}
+                </a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+    </a-spin>
+    <template #footer>
+      <a-button title="保存" type="primary" :loading="loading" @click="saveForm">保存</a-button>
+      <a-button title="返回" type="primary" ghost @click="closeModal">返回</a-button>
+    </template>
+  </AvicModal>
+</template>
+<script lang="ts" setup>
+import { useMdsReportConditionForm, emits } from './ts/MdsReportConditionForm'; // 引入表单ts
+const props = defineProps({
+  formId: {
+    type: String,
+    default: ''
+  }
+});
+const emit = defineEmits(emits);
+const {
+  form,
+  formRef,
+  rules,
+  layout,
+  colLayout,
+  loading,
+  secretLevelList,
+  saveForm,
+  closeModal
+} = useMdsReportConditionForm({
+  props: props,
+  emit: emit
+});
+</script>
+
+
